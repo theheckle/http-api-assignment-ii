11c8f58de1adcea33a82db2809f4d0929b297da9
Sam Heckle <samheckle@yahoo.com>
2018-09-23 17:26:24 -0400

    added initial files

---
+++ src/jsonResponses.js
@@ -0,0 +1,70 @@
+// Note this object is purely in memory
+// When node shuts down this will be cleared.
+// Same when your heroku app shuts down from inactivity
+// We will be working with databases in the next few weeks.
+const users = {};
+
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
+
+  respondJSON(request, response, 200, responseJSON);
+};
+
+const addUser = (request, response, body) => {
+  const responseJSON = {
+    message: 'Name and age are both required',
+  };
+
+  if(!body.name || !body.age){
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  let responseCode = 201;
+
+  if(users[body.name]){
+    responseCode = 204;
+  } else {
+    users[body.name] = {};
+  }
+
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  if(responseCode === 201){
+    responseJSON.message = 'created successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+
+  return respondJSONMeta(request, response, responseCode);
+
+};
+
+const notFound = (request, response, acceptedTypes) => {
+  const responseObj = {
+    id: 'notFound',
+    message: 'The page you are looking for is not found',
+  };
+  
+  const res = JSON.stringify(responseObj);
+  return respondJSON(request, response, 404, 'application/json');
+};
+
+module.exports = {
+  getUsers,
+  addUser,
+  notFound,
+};
---
+++ src/server.js
@@ -0,0 +1,72 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const responseHandler = require('./htmlResponses.js');
+const objectHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': responseHandler.getIndex,
+  '/style.css': responseHandler.getCSS,
+  '/getUsers': objectHandler.getUsers,
+  '/notReal': objectHandler.notFound,
+  '/addUser': objectHandler.addUser,
+  notFound: objectHandler.notFound,
+  index: responseHandler.getIndex,
+};
+
+const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const body = [];
+
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', (chunk) => {
+      const bodyString = Buffer.concat(body).toString();
+      console.dir(bodyString);
+
+      const bodyParams = query.parse(bodyString);
+
+      objectHandler.addUser(request, response, bodyParams);
+    });
+  }
+};
+
+const handleGet = (request, response, parsedUrl) => {
+
+  const acceptedTypes = request.headers.accept.split(',');
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
+  } else {
+    urlStruct.notFound(request, response, acceptedTypes);
+  }
+};
+
+const handleHead = (request, repsonse, parsedUrl) => {
+
+}
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else if (request.method === 'HEAD') {
+    handleHead(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
+};
+
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
